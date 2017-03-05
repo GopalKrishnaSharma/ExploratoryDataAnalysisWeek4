5bc4f0ab1202d5af5f1fffd2798376bb8720c2b4
GopalKrishnaSharma <gopal.krishnasharma@gmail.com>
2017-03-05 17:39:29 +0530

    Plot3 code and output

---
+++ plot3.R
@@ -0,0 +1,18 @@
+#read datasets
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#extract data from Baltimore
+NEI_Baltimore = subset(NEI, NEI$fips == "24510")
+
+#sum the pm2.5 by year and type
+TE_Baltimore_type <- aggregate(NEI_Baltimore[,4], by = list(year = NEI_Baltimore$year,type = NEI_Baltimore$type), FUN = sum)
+
+#load ggplot library
+library(ggplot2)
+
+#plot the data
+png("plot3.png")
+g <- ggplot(TE_Baltimore_type, aes(year, x))
+g + geom_line()+geom_point()+facet_grid(. ~ type) + labs(y = "Total Emission")
+dev.off()
